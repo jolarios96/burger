ebf0bbf436f7f188ce9fd5579380d41587f90f37
Jordan Larios <jolarios96@gmail.com>
2018-12-21 21:57:24 -0800

    schema edit

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,8 @@ USE hamburger_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	eaten BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false
+	PRIMARY KEY (id)
 );
