c8b904d3de4eb329798c8f285957c03556a9b449
Jordan Larios <jolarios96@gmail.com>
2018-12-21 21:21:35 -0800

    blanked password field

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Jbl.92196",
+    password: "",
     database: "hamburger_db"
 });
 
